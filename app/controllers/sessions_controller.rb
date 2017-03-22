d80679e311ed87a6dbd3948c8283723a8695ade1
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-03-22 11:28:11 -0400

    finished assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,22 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to episodes_path
+    else
+      redirect_to '/login', alert: "Incorrect username or password"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :guests, only: [:index]
-  resources :episodes, only: [:index]
+  resources :guests, only: [:index, :show] do
+    resources :appearances, only: [:new,:create]
+  end
+  resources :episodes, only: [:index, :show]
+
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+
+  # also creating a logout for my sanity
+  get '/logout', to: 'sessions#destroy'
 end
